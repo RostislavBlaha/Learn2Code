690b2628065da303c854d01a3517e9994324e314
Rostislav Blaha <Rostislav Blaha>
2016-04-07 22:36:31 +0200

    New method for moving cards between folders. Needs to be tested!

--- src/Libraries/arrayNinja.js
+++ src/Libraries/arrayNinja.js
@@ -50,31 +50,51 @@ export function editItemInArray(array, itemId, url) {
 }
     
 export function moveItemInArray(array, oldID, newID){
-        var newData = array
-        var startCard = ""  
+    var newData = array
+    var startCard = ""  
         
-        for (var i = 0; i < newData.length; i++) {
-            if (newData[i].id == oldID){
-                startCard = newData[i]
-            }
+    for (var i = 0; i < newData.length; i++) {
+        if (newData[i].id == oldID){
+            startCard = newData[i]
         }
-       
-        if (startCard.id < newID){
-                for (var i = 0; i < newData.length; i++) {
-                    if (newData[i].id > startCard.id && newData[i].id <= newID){
-                        newData[i].id = newData[i].id - 1
-                    }
-                }  
-        } else if (startCard.id > newID){   
-                for (var i = 0; i < newData.length; i++) {
-                    if (newData[i].id < startCard.id && newData[i].id >= newID){
-                        newData[i].id++
-                    }
-                }  
-            } 
-        startCard.id = newID
-        newData.sort((a, b) => a.id - b.id)
-        return(
-            newData
-        )
     }
+       
+    if (startCard.id < newID){
+            for (var i = 0; i < newData.length; i++) {
+                if (newData[i].id > startCard.id && newData[i].id <= newID){
+                    newData[i].id = newData[i].id - 1
+                }
+            }  
+    } else if (startCard.id > newID){   
+            for (var i = 0; i < newData.length; i++) {
+                if (newData[i].id < startCard.id && newData[i].id >= newID){
+                    newData[i].id++
+                }
+            }  
+        } 
+    startCard.id = newID
+    newData.sort((a, b) => a.id - b.id)
+    return(
+        newData
+    )
+}
+    
+export function moveItemToAnotherArray(oldArray, newArray, itemId){
+    var id = itemId - 1
+    var item = oldArray[id]
+    var newArrayNewData = newArray
+    var oldArrayNewData = oldArray
+    item.id = newArrayNewData.length + 1
+    newArrayNewData.push(item)
+    oldArrayNewData.filter(function(obj){return (obj.id != id)})
+    for (var i = 0; i < oldArrayNewData.length; i++) {
+        if (oldArrayNewData[i].id > id){
+            oldArrayNewData[i].id--
+        }
+    }
+    return({
+        newArrayNewData: newArrayNewData,
+        oldArrayNewData: oldArrayNewData,
+    })
+    
+}
