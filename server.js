1246fdfe83a5429398366d0cb28fe102bc392eb6
Rosta Blaha <rostislav@rostislavblaha.cz>
2016-05-06 16:16:29 +0200

    Express.js setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var webpack = require('webpack')
+var WebpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
+var express = require('express')
+var app = express()
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -8,8 +10,24 @@ new WebpackDevServer(webpack(config), {
   historyApiFallback: true
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
+    
+  app.get('/', function(req, res) {
+      res.send('hello world')
+  })
+  
+  app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+  
+  app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user')
+  })
+  
+  app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })
 
-  console.log('Listening at localhost:3000');
-});
+  console.log('Listening at localhost:3000')
+})
